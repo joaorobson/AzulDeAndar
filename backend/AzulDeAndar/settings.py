4231d5293e072a4078a1e53254932877532058f7
joaorobson <joaorobsonmartins@gmail.com>
2017-11-17 00:35:39 -0200

    Hide secret key

--- backend/AzulDeAndar/settings.py
+++ backend/AzulDeAndar/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'j*xs_@rynf6wp@5)wndk5yqgy&e21^sl@f_lufdhe7g+^sapq@'
+
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
