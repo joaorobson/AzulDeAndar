72ce3585c1ea4dfa2c04ad9c0509490db72b907d
joaorobson <joaorobsonmartins@gmail.com>
2018-01-28 16:55:01 -0200

    Put StudentList component on index page

--- frontend/assets/js/index.jsx
+++ frontend/assets/js/index.jsx
@@ -1,8 +1,10 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
 import MenuLayout from "./menu/menu"
+import StudentCard from "./components/studentCard"
 import Routes from "./routes/routes"
 import {BrowserRouter} from 'react-router-dom';
+import StudentsList from './pages/studentsList'
 require('../../stylesheet/semantic.css');
 
 
@@ -13,6 +15,7 @@ class App extends React.Component{
       <BrowserRouter>
         <div>
           <MenuLayout />
+          <StudentsList />
           <Routes />
         </div>
       </BrowserRouter>
