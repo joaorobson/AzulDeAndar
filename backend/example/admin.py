f85c8506520dc68b506eb21f20d2f218d670f0e6
joaorobson <joaorobsonmartins@gmail.com>
2018-01-30 22:00:45 -0200

    Add Teacher and Class in django admin

--- backend/example/admin.py
+++ backend/example/admin.py
@@ -1,13 +1,28 @@
 from django.contrib import admin
 from student.models import *
 
+class ClassStudentInline(admin.StackedInline):
+    model = Student.school_class.through 
+
+class TeacherStudentInline(admin.StackedInline):
+    model = Student.teacher.through 
+
 class TelephoneInline(admin.StackedInline):
     model = Telephone
 
+class ClassAdmin(admin.ModelAdmin):
+    inlines = [ClassStudentInline]
+
+class TeacherAdmin(admin.ModelAdmin):
+    inlines = [TeacherStudentInline]
+
 class StudentAdmin(admin.ModelAdmin):
-    inlines = [TelephoneInline]
+    inlines = [TelephoneInline, ClassStudentInline, TeacherStudentInline]
+    exclude = ('school_class','teacher',)
 
 
 
 admin.site.register(Student, StudentAdmin)
 admin.site.register(Class)
+admin.site.register(School)
+admin.site.register(Teacher)
