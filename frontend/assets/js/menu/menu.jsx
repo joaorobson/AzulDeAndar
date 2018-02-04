3259e396ab0c849f89048ed33b7c6e26144fca25
joaorobson <joaorobsonmartins@gmail.com>
2018-02-04 00:07:17 -0200

    Fix menu active items

--- frontend/assets/js/menu/menu.jsx
+++ frontend/assets/js/menu/menu.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
-import {Card, Container, Image, Input, Grid, Menu, Segment } from 'semantic-ui-react'
+import {Card, Container, Image, Input, Grid, Menu, Segment, Icon } from 'semantic-ui-react'
 import {getData} from '../resources/getData.jsx'
+import {Link} from "react-router-dom"
+
 
 
 
@@ -8,23 +10,20 @@ export default class MenuLayout extends Component {
 
  constructor(props) {
 	super(props)
-	this.state = { activeItem: 'alunos' }
-    this.handleItemClick = this.handleItemClick.bind(this)
+	this.state = { "activeItem": window.location.pathname }
 	}
 
-  handleItemClick(e, name) 
-  {console.log(name)
-    this.setState({ activeItem: name.name })}
+  handleItemClick(e, name) {
+    this.setState({ "activeItem": window.location.pathname })
+  }
     
   componentWillReceiveProps = () => {
     this.handleItemClick()
   }
-  load(){
-  const a = '/api/bla/';
-  }
+
   render() {
     const { activeItem } = this.state
-    this.load();
+
     return (
 		<div>
 		<Grid centered>
@@ -32,8 +31,11 @@ export default class MenuLayout extends Component {
 		<Grid.Column width={16}>
 		<Segment style= {{backgroundColor:"#7abdff"}} inverted>
         <Menu inverted secondary attached='top' tabular>
-          <Menu.Item name='alunos' active={activeItem === 'alunos'} onClick={this.handleItemClick} />
-          <Menu.Item name='turmas' active={activeItem === 'turmas'} onClick={this.handleItemClick} />
+          <Menu.Item name='icone' as={Link} to={"/"} onClick={this.handleItemClick} >
+            <Icon name='student' size='large' color='black' />
+          </Menu.Item>
+          <Menu.Item name='alunos' key={"/"} as={Link} to={"/"} active={activeItem === '/'} onClick={this.handleItemClick} />
+          <Menu.Item name='turmas' key={"/classes/"} as={Link} to={"/classes/"} active={activeItem === '/classes/'} onClick={this.handleItemClick} />
           <Menu.Menu position='right'>
             <Menu.Item>
               <Input transparent icon={{ name: 'search', link: true }} placeholder='Search users...' />
