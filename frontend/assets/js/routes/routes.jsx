5fac50a88f18604d699011788e96e9619247445c
joaorobson <joaorobsonmartins@gmail.com>
2018-02-04 00:04:36 -0200

    Add new routes

--- frontend/assets/js/routes/routes.jsx
+++ frontend/assets/js/routes/routes.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 import StudentForm from "../forms/StudentForm";
+import StudentsList from "../pages/studentsList";
+import ClassesList from "../pages/classesList";
+import StudentsByClassList from "../pages/studentsByClassList";
+
 export default class Routes extends React.Component {
 
   render = () => {
@@ -8,7 +12,10 @@ export default class Routes extends React.Component {
     return (
         <div>
             <Switch>
-                <Route exact path="/ok/" component={StudentForm} />
+                <Route exact path="/" component={StudentsList} />
+                <Route exact path="/classes/" component={ClassesList} />
+                <Route path="/classes/:class_name/" component={StudentsByClassList} />
+                <Route render={() => <div><h1>404</h1><h2>Page not found</h2></div>} />
             </Switch>
         </div>
     )
