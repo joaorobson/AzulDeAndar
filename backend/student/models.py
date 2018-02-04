c1635d135f765f39bd07613cb3213bb3babad5f0
joaorobson <joaorobsonmartins@gmail.com>
2018-02-04 00:11:19 -0200

    Add new properties to student app and set the serializers and views up

--- backend/student/models.py
+++ backend/student/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from smartfields import fields
 import datetime
+from  AzulDeAndar import settings
 
 class Teacher(models.Model):
     
@@ -31,6 +32,7 @@ class Class(models.Model):
 
     school = models.ForeignKey(School,on_delete=models.SET_DEFAULT, default=1) 
 
+
     name = models.CharField(
         max_length=30,
         help_text=_("Qual a turma do aluno?"),
@@ -45,8 +47,11 @@ class Class(models.Model):
 
     reference_year = models.IntegerField(choices=YEAR_CHOICES, default=datetime.datetime.now().year)
 
+    class Meta:
+        unique_together = ('name', 'reference_year')
+
     def __str__(self):
-        return str(self.reference_year) + '/' + self.name + "/" + self.school.name 
+        return str(self.reference_year) + ': ' + self.name + "/" + self.school.name 
 
 class Student(models.Model):
 
@@ -55,27 +60,59 @@ class Student(models.Model):
             help_text=_("Qual o nome do aluno?")    
     )
 
+
+    date_of_birth = models.DateField(
+            blank=True,
+            default='1998-03-08',
+    )
+
+    @property
+    def convert_date_of_birth(self):
+        return self.date_of_birth.strftime('%d/%m/%y')
+
+
+    @property
+    def get_telephone_numbers(self):
+        return list(self.telephone_set.all().values_list( \
+                    'telephone_number', flat=True))
+    @property
+    def schools_names(self):
+        list_of_schools = list(self.school_class.all())
+        list_of_schools.sort(key=lambda x:x.reference_year)
+        return [item.__str__() for item in list_of_schools]
+                    
+    @property
+    def get_teachers_names(self):
+        return list(self.teacher.values_list( \
+                    'name', flat=True))
+
     image = fields.ImageField(
             upload_to="images/",
-            help_text=_("Coloque a foto do aluno."),
+            help_text=_("Coloque a foto do(a) aluno(a)."),
             blank=True
     )
 
     father_name = models.CharField(
             max_length=100,
-            help_text=_("Qual o nome do pai do aluno?"),
+            help_text=_("Qual o nome do pai do(a) aluno(a)?"),
             blank=True
     )
 
     mother_name = models.CharField(
             max_length=100,
-            help_text=_("Qual o nome da mãe do aluno?"),
+            help_text=_("Qual o nome da mãe do(a) aluno(a)?"),
             blank=True
+    )
+
+    responsible = models.CharField(
+            max_length=100,
+            help_text=_("Quem é o responsável pelo(a) aluno(a)?"),
+            default="Os pais",
     )
 
     adress = models.CharField(
             max_length=142,
-            help_text=_("Qual o endereço do aluno?"),
+            help_text=_("Qual o endereço do(a) aluno(a)?"),
             blank=True
     )
 
--- backend/student/serializers.py
+++ backend/student/serializers.py
@@ -2,9 +2,36 @@ from rest_framework import serializers
 from .models import *
 
 
-class StudentSerializer(serializers.ModelSerializer):
+class TelephoneSerializer(serializers.ModelSerializer):
 
     class Meta:
+        model = Telephone
+        fields = '__all__'
+
+class ClassSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Class 
+        fields = '__all__' 
+
+
+class StudentSerializer(serializers.ModelSerializer):
+
+    school_class = ClassSerializer(read_only=True, many=True)
+    class Meta:
         model = Student
-        fields = ['name','image']
+        fields = ['name',
+                  'image',
+                  'father_name',
+                  'mother_name',
+                  'responsible',
+                  'adress',
+                  'school_class',
+                  'teacher',
+                  'convert_date_of_birth',
+                  'get_telephone_numbers',
+                  'special_education_needs',
+                  'schools_names',
+                  'get_teachers_names',
+                  ]
 
