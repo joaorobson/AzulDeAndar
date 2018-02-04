8b9678937349031be2a127adaba0878da010a24b
joaorobson <joaorobsonmartins@gmail.com>
2018-02-04 00:05:51 -0200

    Adjusting students data cards

--- frontend/assets/js/components/studentCard.jsx
+++ frontend/assets/js/components/studentCard.jsx
@@ -8,36 +8,47 @@ export default class StudentCard extends Component {
     constructor(props) {
       super(props)
       this.state = {image : ""}
-      console.log(this.props.image)
     }
 
     componentWillReceiveProps = (nextProps) => {
-      console.log(nextProps)
       this.props = nextProps;
 
     }
 
+    mountSchoolHistoric = () => {
+      const schoolHitoricCard = (this.props.data.schools_names).map((school, index) => {
+        return (<Card.Content key={index}>{school}</Card.Content>)
+      })
+      return schoolHitoricCard
+    }
+
     render = () => {
-      console.log("allo")
       return (
-		  <Card>
-			<Image src={this.props.image} />
-			<Card.Content>
-			  <Card.Header>
-			  </Card.Header>
-			  <Card.Meta>
-				<span className='date'>
-				  Joined in 2015
-				</span>
-			  </Card.Meta>
-			</Card.Content>
-			<Card.Content extra>
-		  <a>
-				<Icon name='student' />
-				22 Friends
-			  </a>
-			</Card.Content>
-		  </Card>
+    <Card color='green' style={{maxWidth:500, width:500}}>
+      <Card.Content>
+        <Image floated='left' height="180" width="180" src={this.props.data.image} />
+        <Card.Header>
+					{this.props.data.name}
+        </Card.Header>
+
+
+
+        <Card.Meta>
+          Data de nascimento: {this.props.data.convert_date_of_birth}
+        </Card.Meta>
+     <br></br> 
+      <Card.Content style={{color:"#5f5d5d"}}><strong>Pai:</strong> {this.props.data.father_name}</Card.Content>
+      <Card.Content style={{color:"#5f5d5d"}}><strong>Mãe:</strong> {this.props.data.mother_name}</Card.Content>
+      <Card.Content style={{color:"#5f5d5d"}}><strong>Responsável:</strong> {this.props.data.responsible}</Card.Content>
+      <Card.Content style={{color:"#5f5d5d"}}><strong>Endereço:</strong> {this.props.data.adress}</Card.Content>
+      <Card.Content style={{color:"#5f5d5d"}}><strong>Telefone(s):</strong> {this.props.data.get_telephone_numbers.join("/")}</Card.Content>
+      <Card.Content style={{color:"#5f5d5d"}}><strong>Professor(es):</strong> {this.props.data.get_teachers_names.join(", ")}</Card.Content>
+      <Card.Content style={{color:"#5f5d5d"}}><strong>NEE:</strong> {this.props.data.special_education_needs} </Card.Content>
+      </Card.Content>
+      {this.mountSchoolHistoric()}
+ 
+    </Card>
+
 		)
 	}
 }
